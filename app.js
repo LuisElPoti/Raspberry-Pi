be644bb8108c100a8dad33a1baac6fff26e82902
Luis Adames <adames1601@gmail.com>
2023-08-26 09:57:13 -0400

    Prueba sensores

--- app.js
+++ app.js
@@ -2,37 +2,37 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io');
-//const Gpio = require('onoff').Gpio;
+const sensor = require('node-dht-sensor'); // Import the DHT sensor library
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-//const ledRed = new Gpio(24, 'out');
-//const ledGreen = new Gpio(25, 'out');
 const temperaturaUmbral = 25.0;
-
-let simulatedTemperatura = 20.0; // Initial simulated temperature
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
   console.log('Client connected');
 
-  // Simulate sensor data
+  // Read sensor data
   setInterval(() => {
-    // Fluctuate the simulated temperature slightly
-    simulatedTemperatura += (Math.random() - 0.5) * 2; // Change range here
+    // Read sensor values
+    sensor.read(11, 4, (err, temperature, humidity) => {
+      if (!err) {
+        socket.emit('temperature', temperature.toFixed(2));
+        socket.emit('humidity', humidity.toFixed(2));
 
-    socket.emit('temperature', simulatedTemperatura);
-
-    // if (simulatedTemperatura > temperaturaUmbral) {
-    //   ledRed.writeSync(1);
-    //   ledGreen.writeSync(0);
-    // } else {
-    //   ledRed.writeSync(0);
-    //   ledGreen.writeSync(1);
-    // }
+        // Example LED control based on temperature threshold
+        // if (temperature > temperaturaUmbral) {
+        //   ledRed.writeSync(1);
+        //   ledGreen.writeSync(0);
+        // } else {
+        //   ledRed.writeSync(0);
+        //   ledGreen.writeSync(1);
+        // }
+      }
+    });
   }, 5000);
 
   socket.on('disconnect', () => {
